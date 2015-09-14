96b7df41cecc4dd4bd8cc2f81f5e699da4db732a
root <root@i2b2.localdomain>
2015-09-14 15:34:45 -0400

    PDx assignment: Misc comments.

--- alignment_eval.pl
+++ alignment_eval.pl
@@ -1,9 +1,11 @@
 use warnings;
 use strict;
 
+# Read SAM file command line option
 my $sam_file = $ARGV[0];
 open my $sam_file_fh, '<', $sam_file or die "Could not open file '$sam_file' $!";
 
+# Set total and unaligned counter to zero
 my $total = 0;
 my $unaligned = 0;
 
@@ -19,7 +21,7 @@ foreach my $line ( <$sam_file_fh> ) {
 	$total++;
 	
 }close($sam_file_fh);
-
+# Print error rate
 print "Total unaligned reads: $unaligned\n";
 print "Total reads: $total\n";
 print "Error rate: ($unaligned/$total)*100";
